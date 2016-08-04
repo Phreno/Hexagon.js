4bb22587e6494882d9436add03596c3f9070dde4
K3rn€lP4n1k <losgoomy@gmail.com>
2016-08-05 00:42:01 +0200

    #5: restructuration

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var 
+gulp = require( "gulp" )
+, fs = require( "fs" )
+, coffeelint = require( "gulp-coffeelint" )
+, coffee = require( "gulp-coffee" )
+, concat = require( "gulp-concat" )
+, header = require( "gulp-header" )
+, bundle = require( "./package.json" ); 
+
+/**
+ * Information sur le package.
+ * */
+var about = {
+  template: fs.readFileSync( bundle.config.file.about ),
+  data: {
+    name: bundle.name,
+    version: bundle.version,
+    author: bundle.author,
+    license: fs.readFileSync( bundle.config.file.license ) ,
+    repository: bundle.repository.url,
+    more: "Forked from https://github.com/rrreese/Hexagon.js"
+  }
+};
+
+/**
+ * Liste des taches programmees.
+ * */
+var action = {
+  BUILD: "build",
+  WATCH: "watch"
+};
+
+/**
+ * Generation du livrable.
+ * */
+gulp.task( action.BUILD, function () {
+    gulp
+    .src( bundle.config.filter.src ) 
+    .pipe( coffeelint() )
+    .pipe( coffeelint.reporter( "fail" ))
+    .pipe( concat( bundle.config.file.generated ))
+    .pipe( coffee() )
+    .pipe( header( about.template, about.data ))
+    .pipe( gulp.dest( bundle.config.folder.dist ));
+});
+
+/**
+ * Mise a jour du livrable.
+ * */
+gulp.task( action.WATCH, function () {
+        gulp.watch( 
+          [ bundle.config.filter.src, bundle.config.filter.spec ],  
+          [ action.BUILD ]
+        );
+});
